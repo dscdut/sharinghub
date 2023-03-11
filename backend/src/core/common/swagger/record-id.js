43b2e8c41a354523a621439ff69b590044b7174a
Kiem Tran <kiem.tran@team.enouvo.com>
2023-03-11 22:06:18 +0700

    fix(setup-cicd): test

--- backend/src/core/common/swagger/record-id.js
+++ backend/src/core/common/swagger/record-id.js
@@ -1,8 +1,8 @@
 import { SwaggerDocument } from '../../../packages/swagger';
 
 export const RecordId = SwaggerDocument.ApiParams({
-    name: 'id',
-    paramsIn: 'path',
-    type: 'integer',
-    description: 'Record Id to find',
+  name: 'id',
+  paramsIn: 'path',
+  type: 'integer',
+  description: 'Record Id to find.',
 });
