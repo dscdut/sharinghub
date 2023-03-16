e2f7f61c9a0e88d46bf12be34489ef232ee4bce3
Kreazy <102190042@sv1.dut.udn.vn>
2023-03-16 21:19:19 +0700

    fix: fix UI divider

--- mobile/lib/modules/map/widgets/list_campaigns.widget.dart
+++ mobile/lib/modules/map/widgets/list_campaigns.widget.dart
@@ -17,17 +17,10 @@ class ListCampaigns extends StatelessWidget {
       shrinkWrap: true,
       itemBuilder: (context, index) {
         final CampaignModel currentCampaign = campaigns[index];
-        return Container(
-          color: currentCampaign.isOngoing
-              ? Colors.grey.withOpacity(0.2)
-              : Colors.transparent,
-          padding: const EdgeInsets.symmetric(vertical: 4),
-          child: CampaignItem(currentCampaign: currentCampaign),
-        );
+        return CampaignItem(currentCampaign: currentCampaign);
       },
       separatorBuilder: (context, index) => const Divider(
         color: Colors.transparent,
-        height: 0,
       ),
       itemCount: campaigns.length,
     );
