c8808045e98f0315b8766117b4a08b892926c876
anhquoc2010 <muicamau007@gmail.com>
2023-04-24 02:13:27 +0700

    feat(sha-98): feedback based on account mode

--- mobile/lib/modules/campaign/widgets/detail/campaign_feedback_button.widget.dart
+++ mobile/lib/modules/campaign/widgets/detail/campaign_feedback_button.widget.dart
@@ -1,8 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile/common/widgets/app_rounded_button.widget.dart';
 import 'package:mobile/data/models/campaign.model.dart';
 import 'package:mobile/generated/locale_keys.g.dart';
+import 'package:mobile/modules/auth/auth.dart';
 import 'package:mobile/router/app_routes.dart';
 
 class CampaignFeedbackButton extends StatelessWidget {
@@ -18,11 +20,17 @@ class CampaignFeedbackButton extends StatelessWidget {
     return AppRoundedButton(
       width: double.infinity,
       onPressed: () {
-        Navigator.pushNamed(
-          context,
-          AppRoutes.organizationFeedback,
-          arguments: campaign,
-        );
+        context.read<AuthBloc>().state.status.isAuthenticatedOrganization
+            ? Navigator.pushNamed(
+                context,
+                AppRoutes.organizationFeedback,
+                arguments: campaign,
+              )
+            : Navigator.pushNamed(
+                context,
+                AppRoutes.individualFeedback,
+                arguments: campaign,
+              );
       },
       content: campaign.hasFeedback
           ? LocaleKeys.button_edit_feedback.tr()
