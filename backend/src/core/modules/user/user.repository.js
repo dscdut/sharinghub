a4ca7b3ab5de89a636ac4cd77d282d0ad6b5d025
Kreazy <102190042@sv1.dut.udn.vn>
2023-05-02 08:22:37 +0700

    fix: return user id :fire:

--- backend/src/core/modules/user/user.repository.js
+++ backend/src/core/modules/user/user.repository.js
@@ -63,6 +63,7 @@ class Repository extends DataRepository {
                 'users.birthday',
                 'users.address',
                 'users.workplace',
+                'users.id',
             );
     }
 
