d69ee359a9b0b5f0721079a9585efc5fb7ca83d3
Phạm Hồng Phúc <phamhongphuc1403@gmail.com>
2023-05-19 18:09:35 +0700

    enhance: BE - allow uploading webp files (#136)

--- backend/src/core/modules/document/interceptor/media.interceptor.js
+++ backend/src/core/modules/document/interceptor/media.interceptor.js
@@ -5,7 +5,7 @@ import { MulterUploader } from '../multer.handler';
 export class MediaInterceptor extends BaseMulterInterceptor {
     constructor(fileQuantity = 1) {
         super(new MulterUploader(
-            ['.png', '.jpg', '.jpeg'],
+            ['.png', '.jpg', '.jpeg', 'webp'],
             'image',
             fileQuantity,
             `${ROOT_DIR}/core/uploads/media`
