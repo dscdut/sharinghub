37c8514d0155bd96a549c66b420689c1f75f3a30
Duc Nguyen <beobiebom@gmail.com>
2023-05-23 01:07:26 +0700

    refactor: delete unused code

--- mobile/lib/common/constants/endpoints.dart
+++ mobile/lib/common/constants/endpoints.dart
@@ -20,9 +20,6 @@ abstract class Endpoints {
   // Coordinators
   static String coordinators = '$apiUrl/campaigns/coordinates';
 
-  // static String autocompletePlace =
-  //     'https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyD8y0Rb-Hg6n26z-MP30neOY3yxoBb-Ki0&components=country:vn&region=vn&types=geocode';
-
   static String autocompletePlace =
       'https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${AppFlavor.googleMapApiKey}&components=country:vn&region=vn&types=administrative_area_level_3|administrative_area_level_2';
 
