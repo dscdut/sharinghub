453966493caf47343df9400e44e78cd0edf9ce12
Phạm Hồng Phúc <phamhongphuc1403@gmail.com>
2023-05-26 03:35:54 +0700

    enhance(SHA-108): BE - allow nullable name when searching campaign

--- backend/src/core/modules/campaign/interceptor/coordinate-campaign.interceptor.js
+++ backend/src/core/modules/campaign/interceptor/coordinate-campaign.interceptor.js
@@ -3,7 +3,7 @@ import { DefaultValidatorInterceptor } from 'core/infrastructure/interceptor';
 
 export const CoordinateCampaignInterceptor = new DefaultValidatorInterceptor(
     Joi.object({
-        name: Joi.string().trim().min(1).optional(),
+        name: Joi.string().trim().optional().empty(''),
         lng: Joi.number().optional(),
         lat: Joi.number().optional(),
     }),
